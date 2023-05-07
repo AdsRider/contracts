bf54e060e61a43fe7ce75efe4fcf21fac753e81c
Lurang <lurangjh@naver.com>
2023-05-07 20:57:54 +0900

    AdsRider contract by OpenZeppelin

--- contracts/AdsRider.sol
+++ contracts/AdsRider.sol
@@ -1,7 +1,11 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.9.0;
+pragma solidity ^0.8.17;
 
-contract AdsRider {
-  constructor() public {
-  }
+import "./ERC20.sol";
+
+contract AdsRiderToken is ERC20 {
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) {
+        // 100억개
+        _mint(msg.sender, 10000000000 * 10 ** uint(decimals()));
+    }
 }
